11ff7bf4bb4ca8fceafe1ac3b9c997197ecbc52f
Suchir Ryali <ryalisuchir2.0@gmail.com>
2024-11-09 15:01:34 -0600

    All system rest for between sample collection

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/commands/autonomous/AllSystemRestCommand.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous;
+
+import com.arcrobotics.ftclib.command.InstantCommand;
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+
+import org.firstinspires.ftc.teamcode.common.hardware.Globals;
+import org.firstinspires.ftc.teamcode.common.hardware.RobotHardware;
+
+public class AllSystemRestCommand extends SequentialCommandGroup {
+
+    public AllSystemRestCommand(RobotHardware robot) {
+        super(
+                new ParallelCommandGroup(
+                        new InstantCommand(() -> robot.intakeClawSubsystem.update(Globals.IntakeClawState.OPEN)),
+                        new InstantCommand(() -> robot.intake4BarSubsystem.update(Globals.FourBarState.RESTING)),
+                        new InstantCommand(() -> robot.intakeCoaxialSubsystem.update(Globals.IntakeCoaxialState.REST)),
+                        new InstantCommand(() -> robot.intakeRotationSubsystem.update(Globals.IntakeRotationState.CUSTOM_AUTO, 0.5)),
+                        new InstantCommand(() -> robot.extendoSubsystem.extendoSetPosition(0)),
+                        new InstantCommand(() -> robot.depositSubsystem.outtakeRetract()),
+                        new InstantCommand(() -> robot.outtakeClawSubsystem.update(Globals.OuttakeClawState.OPEN_TRANSFER)),
+                        new InstantCommand(() -> robot.outtakeRotationSubsystem.update(Globals.OuttakeRotationState.TRANSFER)),
+                        new InstantCommand(() -> robot.outtakeArmSubsystem.outtakeArmTransfer())
+                )
+        );
+    }
+}
