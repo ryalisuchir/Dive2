7a1426c42030aff913e3d5b608f75b96ecf4becf
Suchir Ryali <ryalisuchir2.0@gmail.com>
2024-11-17 17:48:56 -0600

    Why do our servo wires keep coming out 😭

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/subsystems/RUN_THIS_FOR_OUTTAKE.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/subsystems/RUN_THIS_FOR_OUTTAKE.java
@@ -33,8 +33,8 @@ public class RUN_THIS_FOR_OUTTAKE extends OpMode {
     @Override
     public void loop() {
         outtakeRotation.setPosition(outtakeRotationPosition);
-        leftOuttakeArm.setPosition(armPositionGeneral);
-        rightOuttakeArm.setPosition(armPositionGeneral);
+//        leftOuttakeArm.setPosition(armPositionGeneral);
+//        rightOuttakeArm.setPosition(armPositionGeneral);
         outtakeClaw.setPosition(outtakeClawPosition);
     }
 }
