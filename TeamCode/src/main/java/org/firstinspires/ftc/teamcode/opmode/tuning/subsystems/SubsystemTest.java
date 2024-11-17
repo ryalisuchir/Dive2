76fba6d51e9e08371e4a91c0c4fc17e2247af77c
Suchir Ryali <ryalisuchir2.0@gmail.com>
2024-11-16 20:55:10 -0600

    New tele-op commands and control.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/subsystems/SubsystemTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmode/tuning/subsystems/SubsystemTest.java
@@ -6,13 +6,13 @@ import com.arcrobotics.ftclib.command.CommandOpMode;
 import com.arcrobotics.ftclib.command.CommandScheduler;
 import com.arcrobotics.ftclib.command.InstantCommand;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Gamepad;
 
 import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.AllSystemInitializeCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.outtake.BucketDropCommand;
-import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.outtake.HighBucketOuttakeCommand;
-import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.transfer.ClawTransferCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.intake.IntakeCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.intake.ScanningCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.outtake.OuttakeCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.transfer.CloseAndTransferCommand;
 import org.firstinspires.ftc.teamcode.common.commandbase.commands.autonomous.outtake.OuttakeTransferReadyCommand;
 import org.firstinspires.ftc.teamcode.common.hardware.Globals;
@@ -21,6 +21,9 @@ import org.firstinspires.ftc.teamcode.common.hardware.RobotHardware;
 @TeleOp
 public class SubsystemTest extends CommandOpMode {
     private RobotHardware robot;
+    private boolean depositManualControl;
+    private boolean extendoManualControl;
+
     double speed;
     @Override
     public void initialize() {
@@ -29,69 +32,139 @@ public class SubsystemTest extends CommandOpMode {
 
     @Override
     public void run() {
-        CommandScheduler.getInstance().run();
+        Gamepad ahnafController = gamepad1;
+        Gamepad swethaController = gamepad1;
 
+        //Loop:
+        CommandScheduler.getInstance().run();
         robot.driveSubsystem.updatePoseEstimate();
         robot.extendoSubsystem.currentLoop();
         robot.extendoSubsystem.extendoSlidesLoop();
-        robot.depositSubsystem.outtakeSlidesLoop();
 
+        //Ahnaf's Controls:
         robot.pinpointDrive.setDrivePowers(new PoseVelocity2d(
                 new Vector2d(
-                        -0.5 * Math.tan(1.12 * gamepad1.left_stick_y),
-                        -0.5 * Math.tan(1.12 * gamepad1.left_stick_x)
+                        -0.5 * Math.tan(1.12 * ahnafController.left_stick_y),
+                        -0.5 * Math.tan(1.12 * ahnafController.left_stick_x)
                 ),
-                -0.5 * Math.tan(1.12 * gamepad1.right_stick_x)
+                -0.5 * Math.tan(1.12 * ahnafController.right_stick_x)
         ));
 
-        boolean triangle = gamepad1.triangle;
-        if (triangle) {
-            schedule (
-                    new AllSystemInitializeCommand(robot)
-            );
-        }
-
-        boolean circle = gamepad1.circle;
-        if (circle) {
-            schedule(
-                    new IntakeCommand(robot, 0.5, 500)
-            );
-        }
-
-        boolean square = gamepad1.square;
-        if (square) {
-            schedule(
-                    new ScanningCommand(robot, 0.5, 500)
-            );
-        }
-
-        boolean x = gamepad1.cross;
-        if (x) {
+        if (ahnafController.cross) {
             schedule(
                     new CloseAndTransferCommand(robot)
             );
         }
 
-        boolean up = gamepad1.dpad_up;
-        if (up) {
-            schedule (
-                    new HighBucketOuttakeCommand(robot)
+        if (ahnafController.circle) {
+            schedule(
+                    new IntakeCommand(robot, 0.5, robot.extendoMotor.getCurrentPosition())
             );
         }
-        boolean down = gamepad1.dpad_down;
-        if (down) {
-            schedule (
+
+        if (ahnafController.square) {
+            schedule(
+                    new ScanningCommand(robot, 0.5, robot.extendoMotor.getCurrentPosition())
+            );
+        }
+
+        if (ahnafController.triangle) {
+            schedule(
                     new BucketDropCommand(robot)
             );
         }
 
-        boolean left = gamepad1.dpad_right;
-        if (left) {
-            schedule (
+        //Swetha's Controls:
+
+        //Extendo Slides Stuff:
+        if (!extendoManualControl) {
+            robot.extendoSubsystem.extendoSlidesLoop();
+        } else {
+            robot.extendoSubsystem.extendoManualControlLoop(swethaController.left_stick_x);
+        }
+
+        if (swethaController.right_stick_y > 0) {
+            extendoManualControl = true;
+        } else if (swethaController.square || swethaController.circle || swethaController.dpad_right || swethaController.dpad_left) {
+            extendoManualControl = false;
+        }
+
+        if (swethaController.circle) {
+            schedule(
+                    new ScanningCommand(robot, 0.5, Globals.EXTENDO_MAX_EXTENSION)
+            );
+        }
+
+        if (swethaController.square) {
+            schedule(
+                    new ScanningCommand(robot, 0.5, ((double) Globals.EXTENDO_MAX_EXTENSION / 2))
+            );
+        }
+
+        if (swethaController.dpad_right) {
+            schedule(
+                    new ScanningCommand(robot, 0.5, ((double) Globals.EXTENDO_MAX_EXTENSION / 4))
+            );
+        }
+
+        if (swethaController.dpad_left) {
+            schedule(
+                    new ScanningCommand(robot, 0.5, Globals.EXTENDO_MAX_RETRACTION)
+            );
+        }
+
+        //Deposit Slides Stuff:
+        if (!depositManualControl) {
+            robot.depositSubsystem.outtakeSlidesLoop();
+        } else {
+            robot.depositSubsystem.depositManualControlLoop(swethaController.right_stick_y);
+        }
+
+        if (swethaController.right_stick_y > 0) {
+            depositManualControl = true;
+        } else if (swethaController.triangle || swethaController.cross || swethaController.dpad_up || swethaController.dpad_down) {
+            depositManualControl = false;
+        }
+
+        if (swethaController.triangle) {
+            schedule(
+              new OuttakeCommand(robot, Globals.LIFT_HIGH_POS)
+            );
+        }
+
+        if (swethaController.cross) {
+            schedule(
+                    new OuttakeCommand(robot, Globals.LIFT_MID_POS)
+            );
+        }
+
+        if (swethaController.dpad_up) {
+            schedule(
+                    new OuttakeCommand(robot, Globals.LIFT_SPECIMEN_POS)
+            );
+        }
+
+        if (swethaController.dpad_down) {
+            schedule(
+                    new OuttakeCommand(robot, Globals.LIFT_RETRACT_POS)
+            );
+        }
+
+        if (swethaController.left_trigger > 0 || swethaController.right_trigger > 0) {
+            schedule(
                     new OuttakeTransferReadyCommand(robot)
             );
         }
 
+
+        //Overrides:
+        if (ahnafController.ps) {
+            ahnafController.rumble(1000);
+            swethaController.rumble(1000);
+            schedule (
+                    new AllSystemInitializeCommand(robot)
+            );
+        }
 
     }
 }
