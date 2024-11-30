96b3776a03d4b6001b032796d9b3f79baf1c3c03
Suchir Ryali <ryalisuchir2.0@gmail.com>
2024-11-30 17:48:01 -0600

    Updated globals for 4 spec

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/Globals.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/Globals.java
@@ -104,7 +104,7 @@ public class Globals {
     public static int LIFT_PARK_POS = 800;
     public static int LIFT_HIGH_POS = 2600;
     public static int LIFT_MID_POS = 1400;
-    public static int LIFT_SPECIMEN_POS = 980;
+    public static int LIFT_SPECIMEN_POS = 960;
     public static int LIFT_SPECIMEN_DROP = 160;
     public static int LIFT_RETRACT_POS = -5;
     public static double LIFT_MAX_TOLERANCE = 5;
@@ -132,6 +132,7 @@ public class Globals {
     public static double OUTTAKE_ARM_BUCKET = 0.63;
     public static double OUTTAKE_ARM_DUNK = 0.62;
     public static double OUTTAKE_ARM_INTAKE = 0.75;
+
     public static double OUTTAKE_ARM_SPECIMEN = 0.66;
     public static double OUTTAKE_ARM_SPECIMEN_LOWER = 0.76;
 
@@ -141,14 +142,14 @@ public class Globals {
     public static double INTAKE_CLAW_SPECIMEN = 0.6;
     public static double INTAKE_CLAW_CLOSED = 0;
 
-    public static double INTAKE_ROTATION_REST = 0.95;
-    public static double INTAKE_ROTATION_TRANSFER = 0.95;
+    public static double INTAKE_ROTATION_REST = 0.39;
+    public static double INTAKE_ROTATION_TRANSFER = 0.39;
 
-    public static double INTAKE_FOURBAR_INTAKE = 0.48;
+    public static double INTAKE_FOURBAR_INTAKE = 0.52;
     public static double INTAKE_FOURBAR_SCANNING = 0.59;
     public static double INTAKE_FOURBAR_LOW = 0.52;
     public static double INTAKE_FOURBAR_TRANSFER = 0.74;
-    public static double INTAKE_FOURBAR_BETWEEN = 0.63;
+    public static double INTAKE_FOURBAR_BETWEEN = 0.60;
     public static double INTAKE_FOURBAR_SPECIMEN = 0.675;
     public static double INTAKE_FOURBAR_RESTING = 0.675;
 
@@ -158,8 +159,8 @@ public class Globals {
     public static double INTAKE_COAXIAL_TRANSFER = 0.93;
 
     //Auto Positions:
-    public static Pose2d DEFAULT_START_POSE = new Pose2d(0, 0, Math.toRadians(0.00));
-    public static Pose2d BLUE_CLOSE_START_POSE = new Pose2d(17, 64, Math.toRadians(270.00));
+    public static Pose2d DEFAULT_START_POSE = new Pose2d(0, 0, Math.toRadians(0));
+    public static Pose2d BLUE_CLOSE_START_POSE = new Pose2d(17, 64, Math.toRadians(270));
     public static Pose2d BLUE_CLOSE_START_POSE_NEW = new Pose2d(17, 64, Math.toRadians(90));
     public static Pose2d BLUE_FAR_START_POSE = new Pose2d(-18, 66.43, Math.toRadians(-90));
 
