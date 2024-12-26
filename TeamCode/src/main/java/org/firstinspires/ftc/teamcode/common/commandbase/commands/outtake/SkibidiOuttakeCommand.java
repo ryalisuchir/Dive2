fccc507e9de0f44fc3a5c9e4ee113ab133a9b41a
Suchir Ryali <ryalisuchir2.0@gmail.com>
2024-12-25 22:52:18 -0600

    Tele-op high bucket

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/commandbase/commands/outtake/SkibidiOuttakeCommand.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.common.commandbase.commands.outtake;
+
+import com.arcrobotics.ftclib.command.ParallelCommandGroup;
+import com.arcrobotics.ftclib.command.SequentialCommandGroup;
+import com.arcrobotics.ftclib.command.WaitCommand;
+
+import org.firstinspires.ftc.teamcode.common.commandbase.commands.regular.OuttakeArmCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.commands.regular.OuttakeClawCommand;
+import org.firstinspires.ftc.teamcode.common.commandbase.commands.slides.DepositSlidesCommand;
+import org.firstinspires.ftc.teamcode.common.hardware.Globals;
+import org.firstinspires.ftc.teamcode.common.hardware.RobotHardware;
+
+public class SkibidiOuttakeCommand extends SequentialCommandGroup {
+
+    public SkibidiOuttakeCommand(RobotHardware robot) {
+        super(
+                new ParallelCommandGroup(
+                        new DepositSlidesCommand(robot.depositSubsystem, Globals.LIFT_HIGH_POS),
+                        new SequentialCommandGroup(
+                                new WaitCommand(600),
+                                new OuttakeArmCommand(robot.outtakeArmSubsystem, Globals.OuttakeArmState.BUCKET)
+                        )
+                )
+        );
+    }
+
+}
