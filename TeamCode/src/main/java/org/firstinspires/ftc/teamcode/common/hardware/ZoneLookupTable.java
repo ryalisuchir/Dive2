e3eac7f703a539b46bb5769f93e3ba426009747d
Suchir Ryali <ryalisuchir2.0@gmail.com>
2025-01-31 22:09:53 -0600

    Zone lookup for human entry

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/ZoneLookupTable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/common/hardware/ZoneLookupTable.java
@@ -9,12 +9,10 @@ public class ZoneLookupTable {
     public ZoneLookupTable() {
         zoneTable = new HashMap<>();
         // Populate with example values
-        zoneTable.put(1, new double[]{3, 0.7});
-        zoneTable.put(2, new double[]{7, 0.7});
-        zoneTable.put(3, new double[]{11, 0.7});
-        zoneTable.put(4, new double[]{3, 0.45});
-        zoneTable.put(5, new double[]{7, 0.45});
-        zoneTable.put(6, new double[]{11, 0.45});
+        zoneTable.put(1, new double[]{-1, 0.65});
+        zoneTable.put(2, new double[]{9, 0.65});
+        zoneTable.put(3, new double[]{-1, 0.45});
+        zoneTable.put(4, new double[]{9, 0.45});
     }
 
     public double[] getZoneInfo(int zone) {
